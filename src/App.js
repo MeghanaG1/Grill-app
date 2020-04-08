77fd55202948f820a2a64247e2504722a9b242dc
MeghanaG1 <61456892+MeghanaG1@users.noreply.github.com>
2020-04-08 16:03:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import AddBanner from './Components/AddBanner';
+import Tile from './Components/Tile';
 import Slider from './Components/Slider';
 import Footer from "./Components/Footer";
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
     <Header />
       <AddBanner />
+      <Tile />
       <Slider />
       <Footer />
     </div>
