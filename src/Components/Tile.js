8074de4fb98c87aced649a807733bffdb7fe714a
MeghanaG1 <61456892+MeghanaG1@users.noreply.github.com>
2020-04-08 16:03:58 +0530

    Add files via upload

---
+++ src/Components/Tile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Tile extends Component {
+    render() {
+        return (
+            <div className="tile-container">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>  
+            <div>4</div>
+          </div>
+        )
+    }
+}
